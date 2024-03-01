9f8bdc618372f7ee6521f29077fe74bcf04c5d23
Mykhailo Popyzenko <mp.and.sp@ukr.net>
2024-03-02 00:40:55 +0200

    delete: comments in contactsSlice.js

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -16,38 +16,6 @@ const initialState = {
   error: null,
 };
 
-// const contactsSlice = createSlice({
-//   name: 'contacts',
-//   initialState,
-//   reducers: {},
-//   extraReducers: {
-//     [fetchContacts.pending]: handlePending,
-//     [addContact.pending]: handlePending,
-//     [deleteContact.pending]: handlePending,
-//     [fetchContacts.rejected]: handleRejected,
-//     [addContact.rejected]: handleRejected,
-//     [deleteContact.rejected]: handleRejected,
-//     [fetchContacts.fulfilled](state, action) {
-//       state.isLoading = false;
-//       state.error = null;
-//       state.items = action.payload;
-//     },
-//     [addContact.fulfilled](state, action) {
-//       state.isLoading = false;
-//       state.error = null;
-//       state.items.push(action.payload);
-//     },
-//     [deleteContact.fulfilled](state, action) {
-//       state.isLoading = false;
-//       state.error = null;
-//       const index = state.items.findIndex(
-//         contact => contact.id === action.payload.id
-//       );
-//       state.items.splice(index, 1);
-//     },
-//   },
-// });
-
 const contactsSlice = createSlice({
   name: 'contacts',
   initialState,
